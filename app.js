a17265a37f1d9cda400c748a2098f9b7b9e8dd0c
louis piquet <louis piquet>
2017-02-03 16:39:12 +0100

    start tp3

--- app.js
+++ app.js
@@ -6,58 +6,40 @@ var cheerio = require( 'cheerio' );
 //creating a new express server
 var app = express();
 
-//setting EJS as the templating engine
 app.set( 'view engine', 'ejs' );
-
-//setting the 'assets' directory as our static assets dir (css, js, img, etc...)
 app.use( '/assets', express.static( 'assets' ) );
+function callLeboncoin() {
+    //creating a var for the url
+    var url = 'https://www.leboncoin.fr/ventes_immobilieres/1087625160.htm?ca=12_s'
 
+    request( url, function ( error, response, html ) {
+        if ( !error && response.statusCode == 200 ) {
+            var $ = cheerio.load( html )
+var lbcDataArray = $( 'section.properties span.value' )
 
+            var lbcData = {
+                price: parseInt( $( lbcDataArray.get( 0 ) ).text().replace( /\s/g, '' ), 10 ),
+                city: $( lbcDataArray.get( 1 ) ).text().trim().toLowerCase().replace( /\_|\s/g, '-' ),
+                type: $( lbcDataArray.get( 2 ) ).text().trim().toLowerCase(),
+ surface: parseInt( $( lbcDataArray.get( 4 ) ).text().replace( /\s/g, '' ), 10 )
+            }
+            console.log( 'data', lbcData )
+
+        }
+        else {
+            console.log( error )
+        }
+    })
+}
 //makes the server respond to the '/' route and serving the 'home.ejs' template in the 'views' directory
 app.get( '/', function ( req, res ) {
+    var url = req.query.urlLBC
+    callLeboncoin();
+ 
     res.render( 'home', {
-        message: 'The Home Page!'
+        message: url,
     });
 });
-function test(){
-    var url = 'https://www.leboncoin.fr/ventes_immobilieres/'
-    request(url, function (error, response, html) {
-        if (!error && response.statusCode == 200) {
-        var $ = cheerio.load(html);
-        var parsedResults = [];
-        $('h3.content').each(function(i, element){
-      // Select the previous element
-      var a = $(this).prev();
-      // Get the rank by parsing the element two levels above the "a" element
-      var rank = a.parent().parent().text();
-      // Parse the link title
-      var title = a.text();
-      // Parse the href attribute from the "a" element
-      var url = a.attr('href');
-      // Get the subtext children from the next row in the HTML table.
-      var subtext = a.parent().parent().next().children('.subtext').children();
-      // Extract the relevant data from the children
-      var points = $(subtext).eq(0).text();
-      var username = $(subtext).eq(1).text();
-      var comments = $(subtext).eq(2).text();
-      // Our parsed meta data object
-      var metadata = {
-        rank: parseInt(rank),
-        title: title,
-        url: url,
-        points: parseInt(points),
-        username: username,
-        comments: parseInt(comments)
-      };
-      // Push meta-data into parsedResults array
-      parsedResults.push(metadata);
-    });
-    // Log our finished parse results in the terminal
-    console.log(parsedResults);
-  }
-})};
-
-
 
 //launch the server on the 3000 port
 app.listen( 3000, function () {
